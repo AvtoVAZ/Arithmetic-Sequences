81fbc5d78442fa70d49e1994cc2ec29dd7f1b2c1
AvtoVAZ <91632854+AvtoVAZ@users.noreply.github.com>
2023-04-09 11:29:52 +0300

    Add files via upload

---
+++ sn.py
@@ -0,0 +1,9 @@
+a1_str = input("What is a1: ")
+a1 = int(a1_str)
+omega_str = input("What is the omega: ")
+omega = int(omega_str)
+n_str = input("What is the n: ")
+n = int(n_str)
+an = (a1+(n-1)*omega)
+sn = (a1+an)*n/2
+print(sn)
